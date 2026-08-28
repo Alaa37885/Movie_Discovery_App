ffccd5a7f55e4caa89c8830fe3dd3abd3a4ab372
Alaa Omar <zain.enzain37@gmail.com>
2026-08-28 15:34:19 +0300

    HomeScreen + Splash

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_nti_aug/cubits/home_cubit/home_cubit.dart';
+// import 'package:movie_nti_aug/cubits/home_cubit/home_state.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+var isShow = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff242A32),
+
+      appBar: AppBar(
+        backgroundColor: const Color(0xff242A32),
+        title: const Text(
+          "What do you want to watch?",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, state) {
+
+            return Column(
+              children: [
+
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    fillColor: const Color(0xff67686D),
+                    filled: true,
+                    suffixIcon: const Icon(Icons.search),
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<HomeCubit>().greet();
+                      isShow = !isShow;
+
+                  },
+                  child: const Text("Get Started"),
+                ),
+
+                if (isShow)
+                  const Text(
+                    "🤙",
+                    style: TextStyle(fontSize: 45),
+                  ),
+
+                const SizedBox(height: 20),
+
+                Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/movie-1.png",
+                      width: 150,
+                      height: 210,
+                    ),
+
+                    const SizedBox(width: 70),
+
+                    Image.asset(
+                      "assets/images/movie-2.png",
+                      width: 150,
+                      height: 210,
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+// Rest API
+// bloc provider على اعلى مستوى branch اول نقطه عندى فى الكود هو object screen
+// bloc make a customize staet neither than set state عشوائية
+// bloc builder اقدر اديها الكمية الل اعملها عادة بناء
+// emit تقوم بعمل اعادة بناء لل state
+// 1 plugin add , add packge (flutter bloc) , bloc provider , bloc builder
+// expanded , listview , builder , separated (sized box)
+
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_nti_aug/screens/home_screen.dart';
+import 'package:movie_nti_aug/cubits/home_cubit/home_cubit.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() {
+    return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    goToHome();
+  }
+
+  void goToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+          () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return BlocProvider(
+                create: (context) {
+                  return HomeCubit();
+                },
+                child: const HomeScreen(),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff242A32),
+
+      body: Center(
+        child: Image.asset(
+          "assets/images/splash.png",
+        ),
+      ),
+    );
+  }
+}
+
